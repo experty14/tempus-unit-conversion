b1e08bdc28f327e89e5a13000e153eebc6317443
shubhamkumargupta <shubhamkumargupta09@gmail.com>
2018-12-19 13:21:52 +0530

    Updating versions of dependency used

--- src/test/java/com/hashmapinc/tempus/QuantityClassSetServiceTest.java
+++ src/test/java/com/hashmapinc/tempus/QuantityClassSetServiceTest.java
@@ -3,9 +3,9 @@ package com.hashmapinc.tempus;
 import com.hashmapinc.tempus.exception.QuantityClassSetException;
 import com.hashmapinc.tempus.exception.UnitConvertorContextException;
 import com.hashmapinc.tempus.service.QuantityClassSetService;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 import java.util.Map;
 import java.util.Set;
@@ -14,46 +14,48 @@ public class QuantityClassSetServiceTest {
 
     private QuantityClassSetService quantityClassSetService;
 
-    @Before
+    @BeforeAll
     public void setUp() throws UnitConvertorContextException {
         quantityClassSetService = UnitConvertorContext.getInstanceOfQuantityClassSetService();
     }
 
     @Test
     public void createQuantityClassSetService() {
-        Assert.assertNotNull(quantityClassSetService);
+        Assertions.assertNotNull(quantityClassSetService);
     }
 
     @Test
     public void getAllQuantityClass() throws QuantityClassSetException {
         Set quantitySetClasses = quantityClassSetService.getAllQuantityClass();
-        Assert.assertNotNull(quantitySetClasses);
-        Assert.assertEquals(175, quantitySetClasses.size());
+        Assertions.assertNotNull(quantitySetClasses);
+        Assertions.assertEquals(175, quantitySetClasses.size());
     }
 
     @Test
     public void getMemberUnitsForQuantityClassForLength() throws QuantityClassSetException{
         Set lengths = quantityClassSetService.getMemberUnitsForQuantityClass("length");
-        Assert.assertNotNull(lengths);
-        Assert.assertEquals(83, lengths.size());
+        Assertions.assertNotNull(lengths);
+        Assertions.assertEquals(83, lengths.size());
     }
 
     @Test
     public void getMemberUnitsForQuantityClassForMass() throws QuantityClassSetException {
         Set masses = quantityClassSetService.getMemberUnitsForQuantityClass("mass");
-        Assert.assertNotNull(masses);
-        Assert.assertEquals(26, masses.size());
+        Assertions.assertNotNull(masses);
+        Assertions.assertEquals(26, masses.size());
     }
 
-    @Test(expected = QuantityClassSetException.class)
+    @Test
     public void getMemberUnitsForQuantityClassForRanadomClassShouldThrowException() throws QuantityClassSetException {
-        quantityClassSetService.getMemberUnitsForQuantityClass("random");
+        Assertions.assertThrows(QuantityClassSetException.class, () -> {
+            quantityClassSetService.getMemberUnitsForQuantityClass("random");
+        });
     }
 
     @Test
     public void  getMemberUnitsForAllQuantityClass() throws QuantityClassSetException {
         Map quantityClassMap = quantityClassSetService.getMemberUnitsForAllQuantityClass();
-        Assert.assertNotNull(quantityClassMap);
-        Assert.assertEquals(175, quantityClassMap.size());
+        Assertions.assertNotNull(quantityClassMap);
+        Assertions.assertEquals(175, quantityClassMap.size());
     }
 }
